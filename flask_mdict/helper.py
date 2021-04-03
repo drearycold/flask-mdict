3a29155537654f0b1db21b85a04a53105f0461ee
Peter Lee <peterli@163.com>
2021-04-03 16:56:21 +0800

    show dicts by order of dirname, remove googletranslate

--- flask_mdict/helper.py
+++ flask_mdict/helper.py
@@ -9,7 +9,7 @@ import logging
 
 from flask import url_for
 
-from googletranslate.googletranslate import main as gtranslate
+# from googletranslate.googletranslate import main as gtranslate
 
 from . import Config, get_db
 from .dbdict_query import DBDict
@@ -235,123 +235,106 @@ def init_mdict(mdict_dir, index_dir=None):
         rows = conn.execute('SELECT name, value FROM setting;')
         for row in rows:
             mdict_setting[row[0]] = row[1] == '1'
-    for root, dirs, files in os.walk(mdict_dir, followlinks=True):
-        for fname in files:
-            if fname.endswith('.db') \
-                    and not fname.endswith('.mdx.db') \
-                    and not fname.endswith('.mdd.db'):
-                db_file = os.path.join(root, fname)
-                d = DBDict(db_file)
-                if not d.is_ok():
-                    continue
-                # mdict db
-                dict_uuid = str(uuid.uuid3(uuid.NAMESPACE_URL, db_file.replace('\\', '/'))).upper()
-                name = os.path.splitext(fname)[0]
-                enable = mdict_setting.get(dict_uuid, True)
-                logger.info('Initialize DICT DB "%s" {%s} [%s]...' % (name, dict_uuid, 'Enable' if enable else 'Disable'))
-                logger.info('\tfind %s:mdx' % fname)
-                if d.is_mdd():
-                    logger.info('\tfind %s:mdd' % fname)
-                logo = 'logo.ico'
-                for ext in ['ico', '.jpg', '.png']:
-                    if os.path.exists(os.path.join(root, name + ext)):
-                        logo = name + ext
-                        break
-                db_names[dict_uuid] = db_file
-                mdicts[dict_uuid] = {
-                    'title': d.title(),
-                    'uuid': dict_uuid,
-                    'logo': logo,
-                    'about': d.about(),
-                    'root_path': root,
-                    'query': d,
-                    'cache': {},
-                    'type': 'mdict_db',
-                    'error': '',
-                    'enable': enable,
-                }
-            elif fname.endswith('.mdx'):
-                name = os.path.splitext(fname)[0]
-                logo = 'logo.ico'
-                for ext in ['ico', '.jpg', '.png']:
-                    if os.path.exists(os.path.join(root, name + ext)):
-                        logo = name + ext
-                        break
-                mdx_file = os.path.join(root, fname)
-                dict_uuid = str(uuid.uuid3(uuid.NAMESPACE_URL, mdx_file.replace('\\', '/'))).upper()
-                enable = mdict_setting.get(dict_uuid, True)
-                logger.info('Initialize MDICT "%s" {%s} [%s]...' % (name, dict_uuid, 'Enable' if enable else 'Disable'))
+    dictdirs = os.listdir(mdict_dir)
+    for dictdir in sorted(dictdirs):
+        for root, dirs, files in os.walk(os.path.join(mdict_dir, dictdir), followlinks=True):
+            logger.info("dirs %s files %s" % (str(sorted(dirs)), str(sorted(files))))
+            for fname in files:
+                if fname.endswith('.db') \
+                        and not fname.endswith('.mdx.db') \
+                        and not fname.endswith('.mdd.db'):
+                    db_file = os.path.join(root, fname)
+                    d = DBDict(db_file)
+                    if not d.is_ok():
+                        continue
+                    # mdict db
+                    dict_uuid = str(uuid.uuid3(uuid.NAMESPACE_URL, db_file.replace('\\', '/'))).upper()
+                    name = os.path.splitext(fname)[0]
+                    enable = mdict_setting.get(dict_uuid, True)
+                    logger.info('Initialize DICT DB "%s" {%s} [%s]...' % (name, dict_uuid, 'Enable' if enable else 'Disable'))
+                    logger.info('\tfind %s:mdx' % fname)
+                    if d.is_mdd():
+                        logger.info('\tfind %s:mdd' % fname)
+                    logo = 'logo.ico'
+                    for ext in ['ico', '.jpg', '.png']:
+                        if os.path.exists(os.path.join(root, name + ext)):
+                            logo = name + ext
+                            break
+                    db_names[dict_uuid] = db_file
+                    mdicts[dict_uuid] = {
+                        'title': d.title(),
+                        'uuid': dict_uuid,
+                        'logo': logo,
+                        'about': d.about(),
+                        'root_path': root,
+                        'query': d,
+                        'cache': {},
+                        'type': 'mdict_db',
+                        'error': '',
+                        'enable': enable,
+                    }
+                elif fname.endswith('.mdx'):
+                    name = os.path.splitext(fname)[0]
+                    logo = 'logo.ico'
+                    for ext in ['ico', '.jpg', '.png']:
+                        if os.path.exists(os.path.join(root, name + ext)):
+                            logo = name + ext
+                            break
+                    mdx_file = os.path.join(root, fname)
+                    dict_uuid = str(uuid.uuid3(uuid.NAMESPACE_URL, mdx_file.replace('\\', '/'))).upper()
+                    enable = mdict_setting.get(dict_uuid, True)
+                    logger.info('Initialize MDICT "%s" {%s} [%s]...' % (name, dict_uuid, 'Enable' if enable else 'Disable'))
 
-                if index_dir:
-                    mdict_index_dir = os.path.join(index_dir, dict_uuid)
-                    if not os.path.exists(mdict_index_dir):
-                        os.makedirs(mdict_index_dir)
-                else:
-                    mdict_index_dir = None
-                idx = IndexBuilder2(mdx_file, index_dir=mdict_index_dir)
-                if not idx._title or idx._title == 'Title (No HTML code allowed)':
-                    title = name
-                else:
-                    title = idx._title
-                    title = regex_tag.sub(' ', title)
+                    if index_dir:
+                        mdict_index_dir = os.path.join(index_dir, dict_uuid)
+                        if not os.path.exists(mdict_index_dir):
+                            os.makedirs(mdict_index_dir)
+                    else:
+                        mdict_index_dir = None
+                    idx = IndexBuilder2(mdx_file, index_dir=mdict_index_dir)
+                    if not idx._title or idx._title == 'Title (No HTML code allowed)':
+                        title = name
+                    else:
+                        title = idx._title
+                        title = regex_tag.sub(' ', title)
 
-                abouts = []
-                abouts.append('<ul>')
-                abouts.append('<li>%s</li>' % os.path.basename(idx._mdx_file))
-                logger.info('\t+ %s' % os.path.basename(idx._mdx_file))
-                for mdd in idx._mdd_files:
-                    abouts.append('<li>%s</li>' % os.path.basename(mdd))
-                    logger.info('\t+ %s' % os.path.basename(mdd))
-                abouts.append('</ul><hr />')
-                if idx._description == '<font size=5 color=red>Paste the description of this product in HTML source code format here</font>':
-                    text = ''
-                else:
-                    text = fix_html(idx._description)
-                about_html = os.path.join(root, 'about_%s.html' % name)
-                if not os.path.exists(about_html):
-                    with open(about_html, 'wt', encoding='utf-8') as f:
-                        f.write(text)
-                if False:
-                    text = regex_style.sub('', text)
-                    text = regex_ln.sub('\n', text)
-                    text = regex_tag.sub(' ', text)
-                    text = [t for t in [t.strip() for t in text.split('\n')] if t]
-                    abouts.append('<p>' + '<br />\n'.join(text) + '</p>')
-                else:
-                    abouts.append(text)
-                about = '\n'.join(abouts)
-                mdicts[dict_uuid] = {
-                    'title': title,
-                    'uuid': dict_uuid,
-                    'logo': logo,
-                    'about': about,
-                    'root_path': root,
-                    'query': idx,
-                    'cache': {},
-                    'type': 'mdict',
-                    'error': '',
-                    'enable': enable,
-                }
-    # for google translate online
-    title = 'Google 翻译'
-    # dict_uuid = str(uuid.uuid3(uuid.NAMESPACE_URL, title)).upper()
-    # fake uuid
-    dict_uuid = 'gtranslate'
-    enable = mdict_setting.get(dict_uuid, True)
-    mdicts[dict_uuid] = {
-        'title': title,
-        'uuid': dict_uuid,
-        'logo': 'google_translate.ico',
-        'about': 'google-translate-for-goldendict<br />https://github.com/xinebf/google-translate-for-goldendict',
-        'root_path': 'translate.google.cn',
-        'query': google_translate,
-        'cache': {},
-        'type': 'app',
-        'error': '',
-        'enable': enable,
-    }
-    db_names[dict_uuid] = None
-    logger.info('Add "%s" [%s]...' % (title, 'Enable' if enable else 'Disable'))
+                    abouts = []
+                    abouts.append('<ul>')
+                    abouts.append('<li>%s</li>' % os.path.basename(idx._mdx_file))
+                    logger.info('\t+ %s' % os.path.basename(idx._mdx_file))
+                    for mdd in idx._mdd_files:
+                        abouts.append('<li>%s</li>' % os.path.basename(mdd))
+                        logger.info('\t+ %s' % os.path.basename(mdd))
+                    abouts.append('</ul><hr />')
+                    if idx._description == '<font size=5 color=red>Paste the description of this product in HTML source code format here</font>':
+                        text = ''
+                    else:
+                        text = fix_html(idx._description)
+                    about_html = os.path.join(root, 'about_%s.html' % name)
+                    if not os.path.exists(about_html):
+                        with open(about_html, 'wt', encoding='utf-8') as f:
+                            f.write(text)
+                    if False:
+                        text = regex_style.sub('', text)
+                        text = regex_ln.sub('\n', text)
+                        text = regex_tag.sub(' ', text)
+                        text = [t for t in [t.strip() for t in text.split('\n')] if t]
+                        abouts.append('<p>' + '<br />\n'.join(text) + '</p>')
+                    else:
+                        abouts.append(text)
+                    about = '\n'.join(abouts)
+                    mdicts[dict_uuid] = {
+                        'title': title,
+                        'uuid': dict_uuid,
+                        'logo': logo,
+                        'about': about,
+                        'root_path': root,
+                        'query': idx,
+                        'cache': {},
+                        'type': 'mdict',
+                        'error': '',
+                        'enable': enable,
+                    }
 
     logger.info('--- MDict is Ready ---')
     return mdicts, db_names
